5b5c560a00ccd09b08e3aba3db35a167e305424d
Alfian Rifai <alfianfirmansyah303@gmail.gmail.com>
2024-01-14 15:49:52 +0700

    Tambahkan file Home, About, Contact, dan ContactForm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import ContactForm from './ContactForm';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/contact" render={() => <ContactForm />} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
